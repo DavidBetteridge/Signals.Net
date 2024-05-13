621c1e5080b5c4a29bd9b860a8b4fbdff026c0a9
davidbetteridge <david.betteridge@zeromission.io>
2024-05-13 08:57:43 +0100

    Add example

---
+++ Signals.Net.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using Signals.Net;
+
+var quantity = new Signal<int>(1);
+var basePrice = new Signal<int>(60);
+var discountedPrice = new Signal<int>(20);
+
+var toPay = new Computed<int>(() => quantity.Get() < 10 ? basePrice.Get() : discountedPrice.Get());
+
+
+Console.WriteLine(toPay.Get());
+Console.ReadKey();
+
+discountedPrice.Set(10);
+Console.ReadKey();
+
+Console.WriteLine(toPay.Get());
+Console.ReadKey();
+
+quantity.Set(11);
+Console.ReadKey();
+
+Console.WriteLine(toPay.Get());
+Console.ReadKey();
