ad81370167b5e87d58d8e228a84bd1e97f7f274f
davidbetteridge <david.betteridge@zeromission.io>
2024-05-14 22:04:35 +0100

    Adults test

--- Signals.Net.Tests/ClassicExamples.cs
+++ Signals.Net.Tests/ClassicExamples.cs
@@ -57,4 +57,48 @@ public class ClassicExamples
         quantity.Set(quantityInBasket);
         Assert.Equal(expectedTotal, toPay.Get());
     }
+
+    [Fact]
+    public void Adults()
+    {
+        var data = Signal.State<List<People>>([new People("David", 48)]); 
+
+        var adults = Signal.Computed(() => data.Get().Where(p => p.Age > 18).ToList(), CompareOrderedLists);
+
+        var computed = false;
+        var numberOfAdults = Signal.Computed(() =>
+        {
+            computed = true;
+            return adults.Get().Count;
+        });
+
+        numberOfAdults.AddEffect((oldCount, newCount) => 
+            Console.WriteLine($"Number of adults changed from {oldCount} to {newCount}"));
+
+        // Doesn't compute
+        computed = false;
+        data.Set([new People("David", 48), new People("Mary", 12)]);
+        Assert.False(computed);
+        
+        // Does compute
+        data.Set([new People("David", 48), new People("Mary", 12), new People("Rebecca", 48)]);
+        Assert.True(computed);
+    }
+    record People(string Name, int Age);
+    
+    bool CompareOrderedLists<T>(IList<T> lhs, IList<T> rhs) where T : notnull
+    {
+        // T must be a record type as they generate the EqualityContract property
+        // There is no proper way to enforce this in C# however.
+        if (lhs.Count != rhs.Count)
+            return false;
+
+        for (var i = 0; i < lhs.Count; i++)
+        {
+            if (!lhs[i].Equals(rhs[i]))
+                return false;
+        }
+
+        return true;
+    }
 }
